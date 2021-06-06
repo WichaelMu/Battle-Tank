4f80a254770f45c5aba9c16f63ac203ff14a97d2
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2021-06-07 09:35:36 +1000

    Basic Tank AI

---
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	ATank* ControlledTank = GetControlledTank();
+	if (ControlledTank) {
+		FString ObjectName = ControlledTank->GetName();
+		ATank* PlayerTank = GetPlayerTank();
+
+		if (PlayerTank) {
+			FString PlayerName = PlayerTank->GetName();
+			UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing %s and found %s"), *ObjectName, *PlayerName);
+		}
+		else {
+			UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing %s but could not find Player"), *ObjectName);
+		}
+
+	}
+
+}
+
+
+ATank* ATankAIController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APawn* PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (PlayerTank) {
+		return Cast<ATank>(PlayerTank);
+	}
+
+	UE_LOG(LogTemp, Error, TEXT("CAN'T FIND PLAYER TANK"));
+
+	return nullptr;
+}
